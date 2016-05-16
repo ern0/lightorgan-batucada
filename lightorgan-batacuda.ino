21550cc68c13622ec138d164b6479c333fb58770
ern0 <ern0@linkbroker.hu>
2016-05-16 18:45:57 +0200

    skeleton created

---
+++ lightorgan-batacuda.ino
@@ -0,0 +1,12 @@
+# if ( defined(__unix__) || defined(__APPLE__) )
+# include "posixino.cpp"
+# endif
+
+	void setup() {
+
+	} // setup()
+
+
+	void loop() {
+
+	} // loop()
---
+++ p
@@ -0,0 +1,2 @@
+clear
+g++ -x c++ -std=c++11 lightorgan-batacuda.ino -o lightorgan-batacuda
