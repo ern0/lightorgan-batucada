c7bb98d23bbd42ac49031cc1919e1c792ee5fc94
ern0 <ern0@linkbroker.hu>
2016-05-18 22:30:42 +0200

    score system works

--- lightorgan-batacuda.ino
+++ lightorgan-batacuda.ino
@@ -1,83 +1,150 @@
 # if ( defined(__unix__) || defined(__APPLE__) )
 # include "../posixino/posixino.cpp"
+
+# define ON (HIGH)
+# define OFF (LOW)
+char xlat[] = { 0,1,2,3,4,5,6,7 };
+
+# else
+
+# define ON (LOW)
+# define OFF (HIGH)
+char xlat[] = { 3,4,5,2,9,6,8,7 };
+
 # endif
 
-char xlat[] = { 0,1,2,3,4,5,6,7 };
-# define R1 0x80
-# define G1 0x40
-# define B1 0x20
-# define W1 0x10
-# define R2 0x08
-# define G2 0x04
-# define B2 0x02
-# define W2 0x01
-# define _ 0x00
+# define SW (ON ^ OFF)
+
+# define r1 0x80
+# define g1 0x40
+# define b1 0x20
+# define w1 0x10
+# define r2 0x08
+# define g2 0x04
+# define b2 0x02
+# define w2 0x01
+# define __ 0x00
+
+unsigned char relayState[8];
+short int delayTime1;
+short int delayTime2;
+unsigned char v1,v2,v3,v4,v5,v6,v7,v8;
+
+
+	void calibrate() {
+
+		while (true) {
+
+			for (int n = 0; n < 8; n++) {
+				digitalWrite(xlat[n],ON);
+				delay(500);
+			}
+
+			delay(1000);
+			for (int n = 0; n < 8; n++) {
+				digitalWrite(xlat[n],OFF);
+			}
+
+			delay(2000);
+
+		} // forever
+
+	} // calibrate()
+
+
+	void d(short int d1,int d2) {
+
+		# if ( defined(__unix__) || defined(__APPLE__) )
+		if (d1 < d2) {
+			printf("bad delay value \n");
+			exit(1);
+		}
+		# endif
+		delayTime1 = d1 - d2;
+		delayTime2 = d2;
+
+	} // d(2)
+
+
+	void d(short int dt) {
+		d(dt,dt >> 1);
+	} // d(1)
+
+
+	void v(unsigned char flip,unsigned char flop) {
+
+		if (flip & r1) relayState[0] ^= SW;
+		if (flip & g1) relayState[1] ^= SW;
+		if (flip & b1) relayState[2] ^= SW;
+		if (flip & w1) relayState[3] ^= SW;
+		if (flip & r2) relayState[4] ^= SW;
+		if (flip & g2) relayState[5] ^= SW;
+		if (flip & b2) relayState[6] ^= SW;
+		if (flip & w2) relayState[7] ^= SW;
+		for (int n = 0; n < 8; n++) digitalWrite(xlat[n],relayState[n]);
+		delay(delayTime1);
+
+		if (flop & r1) relayState[0] ^= SW;
+		if (flop & g1) relayState[1] ^= SW;
+		if (flop & b1) relayState[2] ^= SW;
+		if (flop & w1) relayState[3] ^= SW;
+		if (flop & r2) relayState[4] ^= SW;
+		if (flop & g2) relayState[5] ^= SW;
+		if (flop & b2) relayState[6] ^= SW;
+		if (flop & w2) relayState[7] ^= SW;
+		for (int n = 0; n < 8; n++) digitalWrite(xlat[n],relayState[n]);
+		delay(delayTime2);
+
+	} // v()
+
+
+	# define m(m1,m2,m3,m4,m5,m6,m7,m8) v1=m1;v2=m2;v3=m3;v4=m4;v5=m5;v6=m6;v7=m7;v8=m8;
 
 
 	void setup() {
 
-		for (int n = 0; n < 8; n++) pinMode(xlat[n],OUTPUT);
+		for (int n = 0; n < 8; n++) {
+			pinMode(xlat[n],OUTPUT);
+			relayState[n] = OFF;
+			digitalWrite(xlat[n],relayState[n]);
+		}
+
+		m(r1,g1,b1,w1,r2,g2,b2,w2);
+		d(150,100);
 
 	} // setup()
 
 
-	void tick(short int d,unsigned char data) {
+	void theme1() {
 
-		digitalWrite(xlat[0],( data & 0x80 ? HIGH : LOW ));
-		digitalWrite(xlat[1],( data & 0x40 ? HIGH : LOW ));
-		digitalWrite(xlat[2],( data & 0x20 ? HIGH : LOW ));
-		digitalWrite(xlat[3],( data & 0x10 ? HIGH : LOW ));
-		digitalWrite(xlat[4],( data & 0x08 ? HIGH : LOW ));
-		digitalWrite(xlat[5],( data & 0x04 ? HIGH : LOW ));
-		digitalWrite(xlat[6],( data & 0x02 ? HIGH : LOW ));
-		digitalWrite(xlat[7],( data & 0x01 ? HIGH : LOW ));
-
-		delay(d);
-
-	} // tick()
-
-
-	void test() {
-		tick(100,0);
-		tick(2000,0xff);
-		tick(4000,0);
-	} // test()
+		v( v1 | __ | __ | __ | v5 | __ | __ | __ , v1 | __ | __ | __ | __ | __ | __ | __ );
+		v( __ | __ | __ | __ | __ | __ | __ | __ , __ | __ | __ | __ | __ | __ | __ | __ );
+		v( __ | __ | __ | __ | __ | __ | __ | __ , __ | __ | __ | __ | __ | __ | __ | __ );
+		v( __ | __ | __ | __ | __ | __ | __ | __ , __ | __ | __ | __ | __ | __ | __ | __ );
+		v( v1 | __ | __ | __ | v5 | __ | __ | __ , v1 | __ | __ | __ | __ | __ | __ | __ );
+		v( __ | __ | __ | __ | __ | __ | __ | __ , __ | __ | __ | __ | __ | __ | __ | __ );
+		v( __ | __ | __ | __ | __ | __ | __ | __ , __ | __ | __ | __ | __ | __ | __ | __ );
+		v( v1 | __ | __ | __ | v5 | __ | __ | __ , v1 | __ | __ | __ | __ | __ | __ | __ );
+		v( __ | __ | __ | __ | __ | __ | __ | __ , __ | __ | __ | __ | __ | __ | __ | __ );
+		v( v1 | __ | __ | __ | __ | __ | __ | __ , v1 | __ | __ | __ | __ | __ | __ | __ );
+		v( __ | __ | __ | __ | __ | __ | __ | __ , __ | __ | __ | __ | __ | __ | __ | __ );
+		v( v1 | __ | __ | __ | __ | __ | __ | __ , v1 | __ | __ | __ | __ | __ | __ | __ );
+		v( v1 | __ | __ | __ | v5 | __ | __ | __ , v1 | __ | __ | __ | __ | __ | __ | __ );
+		v( __ | __ | __ | __ | __ | __ | __ | __ , __ | __ | __ | __ | __ | __ | __ | __ );
+		v( v1 | __ | __ | __ | __ | __ | __ | __ , v1 | __ | __ | __ | __ | __ | __ | __ );
+		v( __ | __ | __ | __ | __ | __ | __ | __ , __ | __ | __ | __ | __ | __ | __ | __ );
 
 
-	void intro1(short int d1,int shift) {
 
-		int d2 = d1 + d1;
-		int d3 = d1 + d2;
-
-		tick(d1, W1 >> shift);
-		tick(d1, _ );
-		tick(d1, W1 >> shift);
-		tick(d3, _ );
-
-		tick(d1, W1 >> shift);
-		tick(d1, _ );
-		tick(d1, W1 >> shift);
-		tick(d3, _ );
-
-		tick(d1, W1 >> shift);
-		tick(d1, _ );
-		tick(d1, W1 >> shift);
-		tick(d1, _ );
-		tick(d1, W1 >> shift);
-		tick(d1, _ );
-		tick(d1, W1 >> shift);
-		tick(d1, _ );
-		tick(d1, W1 >> shift);
-		tick(d3, _ );
-
-	} // intro1()
+	}
 
 
 	void loop() {
 
-		//test();
-		intro1(200,0);
-		intro1(200,4);
+		d(150,120);
+		m(r1,r2,0,0,0,0,0,0);
+		theme1();
+		m(r1,r2,w1|w2,0,0,0,0,0);
+		theme1();
 
-
-	} // loop()
+	}
