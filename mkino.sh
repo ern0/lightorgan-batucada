364c16d617d89033b10341b850fe191a2227a6ba
ern0 <ern0@linkbroker.hu>
2016-05-23 22:29:15 +0200

    mut hack added

--- mkino.sh
+++ mkino.sh
@@ -1,2 +1,2 @@
-tr < song/batacuda.html \>\< "\n\n"|grep batacuda > song/batacuda.inc
+cat song/batacuda.html | tr \>\< "\n\n"| grep batacuda | tr -cd '\11\12\15\40-\176' > song/batacuda.inc
 cat song/lightorgan.inc song/batacuda.inc song/patterns.inc > lightorgan-batacuda.ino
